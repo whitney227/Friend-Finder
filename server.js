fb0cedbaeb247f20a70aa9bacc4ee0ce7c637635
whitney227 <whitneylcrawford@gmail.com>
2019-05-19 13:55:57 -0400

    changed the bodyParser

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//create application with JSON parser
-var jsonParser = bodyParser.json()
-
 //set up to handle data parsing
-var urlencodedParser = bodyParser.urlencoded({extended: false})
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json({ type: 'application/*+json'}));
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
